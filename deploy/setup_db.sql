5da6627c0d05f46ea4f13fa5563aa5cc1d0c2e50
svyatoslavmamzin <yoko11.06.92@yandex.ru>
2022-07-03 21:35:50 +0400

    подготовка к развертыванию

--- deploy/setup_db.sql
+++ deploy/setup_db.sql
@@ -3,4 +3,4 @@ ALTER USER :user CREATEDB;
 ALTER USER :user SET client_encoding TO 'utf8';
 ALTER USER :user SET default_transaction_isolation TO 'read committed';
 ALTER USER :user SET timezone TO 'UTC';
-CREATE DATABASE hasker_db OWNER :user;
+CREATE DATABASE hasker OWNER :user;
