a2257759f1553bdc595502931a5d5f30449d3b6d
svyatoslavmamzin <yoko11.06.92@yandex.ru>
2022-07-03 22:52:53 +0400

    add README.md

--- deploy/setup_db.sql
+++ deploy/setup_db.sql
@@ -1,6 +1,6 @@
-CREATE USER :user_db WITH PASSWORD '123456789';
-ALTER USER :user CREATEDB;
-ALTER USER :user SET client_encoding TO 'utf8';
-ALTER USER :user SET default_transaction_isolation TO 'read committed';
-ALTER USER :user SET timezone TO 'UTC';
+CREATE USER user_db WITH PASSWORD '123456789';
+ALTER USER :user_db CREATEDB;
+ALTER USER :user_db SET client_encoding TO 'utf8';
+ALTER USER :user_db SET default_transaction_isolation TO 'read committed';
+ALTER USER :user_db SET timezone TO 'UTC';
 CREATE DATABASE hasker OWNER :user;
